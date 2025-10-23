246e756e4d91e38a12e8901234219725f8071206
PierrePicard20 <pierre.picard20@gmail.com>
2025-10-23 21:47:00 +0200

    refactor: removal of class TaskNode type - fix compilation error after merge

--- End2EndTests/ArmoniK.EndToEndTests.Worker/Tests/FizzBuzzWorker.cs
+++ End2EndTests/ArmoniK.EndToEndTests.Worker/Tests/FizzBuzzWorker.cs
@@ -16,7 +16,6 @@
 
 using System.Text;
 
-using ArmoniK.Api.gRPC.V1;
 using ArmoniK.Extension.CSharp.DllCommon;
 
 using Microsoft.Extensions.Logging;
@@ -28,9 +27,9 @@ public class FizzBuzzWorker : IWorker
   public Task<HealthCheckResult> CheckHealth(CancellationToken cancellationToken = default)
     => Task.FromResult(HealthCheckResult.Healthy());
 
-  public async Task<Output> ExecuteAsync(UserTaskHandler   taskHandler,
-                                         ILogger           logger,
-                                         CancellationToken cancellationToken)
+  public async Task<TaskResult> ExecuteAsync(SdkTaskHandler    taskHandler,
+                                             ILogger           logger,
+                                             CancellationToken cancellationToken)
   {
     var value = taskHandler.GetIntDependency("value");
 
@@ -61,9 +60,6 @@ public class FizzBuzzWorker : IWorker
     await taskHandler.SendResultByNameAsync("result",
                                             Encoding.ASCII.GetBytes(result))
                      .ConfigureAwait(false);
-    return new Output
-           {
-             Ok = new Empty(),
-           };
+    return TaskResult.Success;
   }
 }
